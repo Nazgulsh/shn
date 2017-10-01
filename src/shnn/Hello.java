f9a887aebfe4e9be2bf0996b1477c10ea85039f6
Nazgul <nazgulkyrgyzbay@gmail.com>
2017-10-02 02:01:46 +0600

    ekinshi commit

--- src/shnn/Hello.java
+++ src/shnn/Hello.java
@@ -3,5 +3,7 @@ package shnn;
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello Sh");
+        System.out.print("Бул зертхананын максаты ");
+        System.out.print("GitHub - ты уйрену");
     }
 }
